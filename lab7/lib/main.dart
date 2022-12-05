50dfaa10f1dd60071e6f4cd50a3c2fabc7963beb
RahafCS <105695103+RahafCS@users.noreply.github.com>
2022-12-05 09:44:22 +0300

    Rahaf

---
+++ lab7/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lab7/accountPage.dart';
+import 'package:lab7/postPage.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: accountPage(),
+    );
+  }
+}
